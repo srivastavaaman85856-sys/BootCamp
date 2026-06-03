d6ceadc32b8fd12a6e29d6a753125708c5047bec
Aman Srivastava <srivastavaaman.85856@gmail.com>
2026-06-03 21:42:52 +0530

    Add files via upload

---
+++ Day 3/string compression.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class stringcompression{
+    public static void main(String[] args){
+        Scanner sc  = new Scanner(System.in);
+        String str = sc.nextLine();
+        String result = "";
+        int count =1;
+        for(int i=0;i<str.length()-1;i++){
+            if(str.charAt(i) == str.charAt(i+1)){
+                count++;
+            }
+            else{
+                result = result + str.charAt(i) + count;
+                count=1;
+            }
+
+        }
+        
+        result = result + str.charAt(str.length()-1) + count;
+        System.out.println(result);
+    }
+}
